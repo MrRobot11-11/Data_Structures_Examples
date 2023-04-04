da8a7f3360553096757951055b9d5a0b85a8c62d
Steve <mrr0obot11.11@protonmail.com>
2023-04-04 23:18:49 +0000

    Add files via upload

---
+++ Stack_Queue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lantz_assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            View v = new();
+           
+            //Controller cont = new Controller();
+
+            //cont.RunProgram();
+
+        }
+    }
+}
---
+++ Stack_Queue/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lantz_assignment_4
+{
+    class Test
+    {
+
+       public void TestSwitch()
+        {
+            switch (true)
+            {
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
