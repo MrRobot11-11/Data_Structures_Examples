6c5f180d31812fe5d24864cce2b31ee2b3682c45
Steve <mrr0obot11.11@protonmail.com>
2023-04-04 23:33:52 +0000

    Add files via upload

---
+++ Stack_Queue/Controller.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+
+namespace Lantz_assignment_4
+{
+    class Controller
+    {
+        public void RunProgram()
+        {
+            int choice, input;
+            string typeOfArray = " "; //Queue or stack
+            string stackMenu = "1. Add to the stack\n " +
+                            "2. Pop from the stack\n " +
+                            "3. Peek at the top element\n" +
+                            "4. Display entire stack\n" +
+                            "5. Number of elements in the stack\n";
+
+            Stack stack = new();
+            View view = new();
+            Queue q = new();
+            
+            
+            
+                try
+            {       //First we ask the user what type of array to create: stack/queue
+                    choice = view.GetArrayType();
+
+                while (true)
+                    {
+                        
+
+                  
+
+                    if (choice == 1) 
+                        {
+                            typeOfArray = "stack";
+                        
+                        }
+                        if (choice == 2)
+                        {
+                            typeOfArray = "queue";
+                        
+                        }
+
+                        
+                    if (choice == 6)
+                        {
+                            WriteLine("Goodbye...");
+                            break;
+                        }
+                    Console.WriteLine(choice);
+
+                        switch (view.SelectMenu(choice))
+                        {
+                            case 1:
+                            WriteLine("What would you like to put on the {0}?\n", typeOfArray);
+                            input = Convert.ToInt32(ReadLine());
+                            
+                            switch (choice)
+                            {
+                                case 1:
+                                    WriteLine("got to nested switch!");
+                                    //stack.Push(input);
+                                    break;
+
+                                                   
+                            }
+                            break;
+
+                            case 2:
+                                choice = stack.Pop();
+                                WriteLine("You popped " + choice + " off the stack\n");
+                                break;
+
+                            case 3:
+                                WriteLine("You peeked at the top element which is: " + stack.Peek() + "\n");
+                                break;
+
+                            case 4:
+                                stack.Display();
+                                WriteLine();
+                                break;
+
+                            case 5:
+                                WriteLine("Your stack has " + stack.Size() + " elements\n");
+                                break;
+
+                            default:
+                                WriteLine("Goodbye");
+                                break;
+
+                        }
+                    }//End of while
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("An error has occured.\n" + ex.Message + "\n" + ex.StackTrace);
+                WriteLine("Broke it");
+                Read();
+                }
+
+
+            WriteLine("Broke out the while");
+            Read();
+        }//End of RunProgram
+    }//End of class
+}
---
+++ Stack_Queue/Queue.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lantz_assignment_4
+{
+    class Queue
+    {
+        private static int front, back, capacity = 5;
+        private static int[] queueArray;
+
+        //Constructor to set values
+        public Queue()
+        {
+            front = back = 0;
+            queueArray = new int[capacity];
+        }
+    
+        //Method to insert an element at the rear of the queue
+        public void queueEnqueue(int data)
+        {
+            //Check if queue is full
+            if(capacity == back)
+            {
+                Write("\nQueue is empty\n");
+                return;
+            }
+
+            //Insert element at the back of queue
+            else
+            {
+                queueArray[back] = data;
+                back++;
+            }
+            return;
+        }
+    
+        //Method to delete an element from the front of the queue
+        public void queueDequeue()
+        {
+            //If queue is empty
+            if(front == back)
+            {
+                WriteLine("\nQueue is empty");
+                return;
+            }
+
+            //Shift all elements from index 22 till back to the right by one
+            else
+            {
+                for (int i = 0; i < back; i++)
+                    queueArray[i] = queueArray[i + 1];
+
+                //Store 0 at back indicating there's no element
+                if (back < capacity)
+                    queueArray[back] = 0;
+
+                back--;
+            }
+            return;
+        }
+    
+        //Method to display all elements in the Queue
+        public void queueDisplay()
+        {
+            if(front == back)
+            {
+                WriteLine("\nQueue is empty");
+                return;
+            }
+
+            //Traverse front to rear and print elements
+            for (int i = front; i < back; i++)
+                Write(" {0} <-- ", queueArray[i]);
+
+            return;
+        }
+    
+        //Show front of Queue
+        public void queueFront()
+        {
+            if(front == back)
+            {
+                WriteLine("\nQueue is empty");
+                return;
+            }
+            Write("\nFront element is: " + queueArray[front]);
+            return;
+        }
+
+        //Show number of elements in the queue
+        public int QueueSize()
+        {
+            return back - 1;
+        }
+    
+    
+    
+    }//End of class
+}
